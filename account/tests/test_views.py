71c4a61b7549ba330f5191ee2b3cc6c5a3b0fa80
David Duefrene <fortismilite@gmail.com>
2019-12-15 08:23:24 -0700

    updated test_bad_user_login
    updated test_bad_user_login in UserLoginViewTest, test now actually use the log in view instead of the build in login function.
    
    adjusted admin account allowing it change user first name as well

--- account/tests/test_views.py
+++ account/tests/test_views.py
@@ -3,7 +3,7 @@
 ### account application views testing suite
 ### Copyright 2019 David J Duefrene, All rights reserved.
 """
-from django.test import TestCase
+from django.test import TestCase, Client
 
 class UserLoginViewTest(TestCase):
     fixtures = ['data_dump.json']
@@ -18,44 +18,67 @@ class UserLoginViewTest(TestCase):
 
     def test_good_user_login(self):
         """
-        Test to see if a good uer/pw will work
+        Test to see if a good uer/pw will redirect
         """
-        self.assertTrue(self.client.login(username='alfred', password='Hads65ads1'))
-        self.assertTrue(self.is_user_active())
+        c = Client()
+        response = c.post('/account/login/', {'username': 'alfred',
+                            'password': 'Hads65ads1'}, follow=True)
+        self.assertTrue(response.status_code, 200)
+        self.assertIn(bytes("Hello Alfred", 'utf-8'), response.content)
+
+    def test_login_view(self):
+        response = self.client.get('/account/login/')
+        self.assertTemplateUsed(response, 'registration/login.html')
 
     def test_bad_user_login(self):
         """
         Test to see if missing any info on the form will allow a login
         as well as testing a bad user/pw
         """
+        # Testing missing username
+        c = Client()
+        response = c.post('/account/login/', {'username': '',
+                            'password': 'Hads65ads1'})
+        self.assertIn(bytes("<ul class=\"errorlist\"><li>This field is required.</li></ul>\n<p><label for=\"id_username\">Username:</label>",
+                            'utf-8'), response.content)
+
         # Missing password, good user
-        self.assertFalse(self.client.login(username='alfred', password=''))
-        self.assertFalse(self.is_user_active())
+        c = Client()
+        response = c.post('/account/login/', {'username': 'alfred',
+                            'password': ''})
+        self.assertIn(bytes("<ul class=\"errorlist\"><li>This field is required.</li></ul>\n<p><label for=\"id_password\">Password:</label>",
+                            'utf-8'), response.content)
 
-        # Missing user, good pw
-        self.assertFalse(self.client.login(username='', password='Hads65ads1'))
-        self.assertFalse(self.is_user_active())
+        # Testing missing bioth fields
+        c = Client()
+        response = c.post('/account/login/', {'username': '', 'password': ''})
+        self.assertIn(bytes("<ul class=\"errorlist\"><li>This field is required.</li></ul>\n<p><label for=\"id_password\">Password:</label>",
+                            'utf-8'), response.content)
+        self.assertIn(bytes("<ul class=\"errorlist\"><li>This field is required.</li></ul>\n<p><label for=\"id_username\">Username:</label>",
+                            'utf-8'), response.content)
 
-        # Missing both
-        self.assertFalse(self.client.login(username='', password=''))
-        self.assertFalse(self.is_user_active())
-
-        # Bad LoginForm
-        self.assertFalse(self.client.login(username='bad', password='bad'))
-        self.assertFalse(self.is_user_active())
+        # Test bad login info
+        c = Client()
+        response = c.post('/account/login/', {'username': 'bad', 'password': 'bad'})
+        self.assertIn(bytes("<ul class=\"errorlist nonfield\"><li>Please enter a correct username and password. Note that both fields may be case-sensitive.</li></ul>",
+                            'utf-8'), response.content)
 
     def test_inactive_user_login(self):
         """
         Test to see if a inactive user can login
         """
-        # Test inactive user
-        self.assertFalse(self.client.login(username='name', password='nfghT56'))
-        self.assertFalse(self.is_user_active())
+        c = Client()
+        response = c.post('/account/login/', {'username': 'name', 'password': 'nfghT56'})
+        self.assertIn(bytes("<ul class=\"errorlist nonfield\"><li>Please enter a correct username and password. Note that both fields may be case-sensitive.</li></ul>",
+                            'utf-8'), response.content)
 
 class UserDashboardTest(TestCase):
     fixtures = ['data_dump.json']
 
     def test_user_dashboard(self):
+        """
+        Tests the user dashboard
+        """
         self.assertTrue(self.client.login(username='alfred', password='Hads65ads1'))
         request = self.client.get('/account/')
-        self.assertTrue(request)
+        self.assertTemplateUsed(request, 'account/dashboard.html')
