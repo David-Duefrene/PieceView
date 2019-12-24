817a9330a32b14fcea223134362c7a77c2fafba8
David Duefrene <fortismilite@gmail.com>
2019-12-24 03:40:07 -0700

    fixed templates
    Switched templates to be more along with default Django.
    Moved register and register_done to register folder
    Change account folder to user folder
    Adjusted views in account view.py
    Links option to change user password in dashboard

--- account/views.py
+++ account/views.py
@@ -37,14 +37,14 @@ def register(request):
 		  new_user.set_password(user_form.cleaned_data['password'])
 	  # Save the user
 		  new_user.save()
-		  return render(request, 'account/register_done.html',
+		  return render(request, 'registration/register_done.html',
 					{'new_user': new_user})
   else:
 	  user_form = UserRegistrationForm()
-  return render(request, 'account/register.html',
+  return render(request, 'registration/register.html',
 				  {'user_form': user_form})
 
 @login_required
 def dashboard(request):
-	return render(request, 'account/dashboard.html',
+	return render(request, 'user/dashboard.html',
 				  {'section': 'dashboard'})
