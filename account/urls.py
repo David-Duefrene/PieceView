59f37e584a7ff012a3c68fbca8b29720894f2c95
David Duefrene <fortismilite@gmail.com>
2019-12-28 06:41:44 -0700

    redirect if already logged in
    Created a redirect if user tries to get to the log-in screen while already logged in. to accomplish this I had to re-enable all my customer views including: login, logout, password_change, password_change_done, password_reset, password_reset_done, password_reset_confirm, password_reset_complete.
    As a result of the above I had to recreate a login error for when user put a invalid username or password. PieceView will redirect to ‘login_invalid.html’
    Also cleaned up some inconsistent spacing in views.py register function

--- account/urls.py
+++ account/urls.py
@@ -10,8 +10,17 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    # Default authentication URL
-    path('', include('django.contrib.auth.urls')),
+    # previous login/logout views
+    path('login/', views.user_login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # change password urls
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # reset password urls
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     # Registration URL
     path('register/', views.register, name='register'),
     # Edit Profile
--- account/views.py
+++ account/views.py
@@ -7,47 +7,46 @@ from django.contrib.auth import get_user_model
 from .forms import LoginForm, UserRegistrationForm, UserEditForm
 
 def user_login(request):
+	if request.user.is_authenticated:
+		return render(request, 'user/dashboard.html', {'section': 'dashboard'})
 	if request.method == 'POST':
 		form = LoginForm(request.POST)
 		if form.is_valid():
 			cd = form.cleaned_data
-			user = authenticate(request,
-								 username=cd['username'],
+			user = authenticate(request, username=cd['username'],
 								  password=cd['password'])
 			if user is not None:
 				if user.is_active:
 					login(request, user)
-					return HttpResponse('Authenticated '\
-										'successfully')
+					return render(request, 'user/dashboard.html', {'section': 'dashboard'})
 				else:
 					return HttpResponse('Disabled account')
 			else:
-				return HttpResponse('Invalid Login')
+				return render(request, 'registration/login_invalid.html', {'form': form})
 	else:
 		form = LoginForm()
 	return render(request, 'registration/login.html', {'form': form})
 
 def register(request):
-  if request.method == 'POST':
-	  user_form = UserRegistrationForm(request.POST)
-	  if user_form.is_valid():
-	  # Create a new user object but don't save it
-		  new_user = user_form.save(commit=False)
-	  # Set the chosen password
-		  new_user.set_password(user_form.cleaned_data['password'])
-	  # Save the user
-		  new_user.save()
-		  return render(request, 'registration/register_done.html',
-					{'new_user': new_user})
-  else:
-	  user_form = UserRegistrationForm()
-  return render(request, 'registration/register.html',
+	if request.method == 'POST':
+		user_form = UserRegistrationForm(request.POST)
+		if user_form.is_valid():
+			# Create a new user object but don't save it
+			new_user = user_form.save(commit=False)
+			# Set the chosen password
+			new_user.set_password(user_form.cleaned_data['password'])
+			# Save the user
+			new_user.save()
+			return render(request, 'registration/register_done.html',
+							{'new_user': new_user})
+	else:
+		user_form = UserRegistrationForm()
+	return render(request, 'registration/register.html',
 				  {'user_form': user_form})
 
 @login_required
 def dashboard(request):
-	return render(request, 'user/dashboard.html',
-				  {'section': 'dashboard'})
+	return render(request, 'user/dashboard.html', {'section': 'dashboard'})
 
 @login_required
 def edit(request):
