89f85faee170243bf556f869b0606d3310c4ff39
David Duefrene <fortismilite@gmail.com>
2020-01-01 02:35:13 -0700

    reorganized templates and static files
    reorganized templates and static files to be in the root of the project folder

--- PieceView/settings.py
+++ PieceView/settings.py
@@ -17,7 +17,16 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_ROOT = (os.path.join(BASE_DIR, "static"))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+STATICFILES_ROOT = ''
+STATIC_URL = '/static/'
+STATIC_DIR = (os.path.join(BASE_DIR, 'static'),)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
+MEDIA_URL = '/media/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -59,7 +68,7 @@ ROOT_URLCONF = 'PieceView.urls'
 TEMPLATES = [
 	{
 		'BACKEND': 'django.template.backends.django.DjangoTemplates',
-		'DIRS': ['./templates'], # TODO needed?
+		'DIRS': [TEMPLATE_DIR],
 		'APP_DIRS': True,
 		'OPTIONS': {
 			'context_processors': [
@@ -109,11 +118,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 # Log in/out URL shortcuts
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
