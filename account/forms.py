79ea10de7ae8f9d521449e2c9c45204f71921a9b
David Duefrene <fortismilite@gmail.com>
2020-01-07 05:42:17 -0700

    Update forms.py
    made forms.py more PEP8 complaint with proper indentation etc

--- account/forms.py
+++ account/forms.py
@@ -2,10 +2,16 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import BaseUserManager
 
+
 class UserManager(BaseUserManager):
     pass
 
+
 class UserRegistrationForm(forms.ModelForm):
+    """
+    Form fora new user to register on the site.
+    required fields are username, email, and first name
+    """
     password = forms.CharField(label='Password',
                                widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repeat password',
@@ -21,18 +27,15 @@ class UserRegistrationForm(forms.ModelForm):
         self.fields['first_name'].required = True
 
     def clean_password2(self):
-        """
-        ensures both passwords match
-        """
+        """ensures both passwords match"""
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Passwords don\'t match.', code='mismatch_pass')
+            raise forms.ValidationError('Passwords don\'t match.',
+                code='mismatch_pass')
         return cd['password2']
 
     def save(self, commit=True):
-        """
-        saves the form to the database
-        """
+        """saves the form to the database"""
         # Save the provided password in hashed format
         user = super().save(commit=False)
         user.set_password(self.cleaned_data["password"])
@@ -40,11 +43,18 @@ class UserRegistrationForm(forms.ModelForm):
             user.save()
         return user
 
+
 class UserEditForm(forms.ModelForm):
+        """
+        Form for a user to edit thier account.
+        required fields are username, email, and first name just as
+        UserRegistrationForm
+        """
     class Meta:
         model = get_user_model()
-        exclude = ('last_login', 'is_superuser', 'groups', 'user_permissions', 'is_staff',
-                    'is_active', 'date_joined', 'password', 'username', 'contacts')
+        exclude = ('last_login', 'is_superuser', 'groups', 'user_permissions',
+            'is_staff', 'is_active', 'date_joined', 'password', 'username',
+            'contacts')
 
     def __init__(self, *args, **kwargs):
         super(UserEditForm, self).__init__(*args, **kwargs)
