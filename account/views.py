c3fbf9e7b457447a37aa88c2d2cd50c017972a5c
David Duefrene <fortismilite@gmail.com>
2020-01-07 05:02:03 -0700

    pep8
    converted tabs to spaces in account/views.py

--- account/views.py
+++ account/views.py
@@ -9,60 +9,60 @@ from .forms import UserRegistrationForm, UserEditForm
 from .models import CustomUser, Contact
 
 class UserRegisterCreateView(CreateView):
-	"""
-	Our User registration View. overrides form_valid to render a template
-	"""
-	model = CustomUser
-	form_class = UserRegistrationForm
-	success_url = 'registration/register_done.html'
-	template_name = 'registration/register.html'
+    """
+    Our User registration View. overrides form_valid to render a template
+    """
+    model = CustomUser
+    form_class = UserRegistrationForm
+    success_url = 'registration/register_done.html'
+    template_name = 'registration/register.html'
 
-	def form_valid(self, form):
-		form.save()
-		return render(self.request, 'registration/register_done.html', self.get_context_data())
+    def form_valid(self, form):
+    form.save()
+    return render(self.request, 'registration/register_done.html', self.get_context_data())
 
 @login_required
 def dashboard(request):
-	return render(request, 'user/dashboard.html', {'section': 'dashboard'})
+    return render(request, 'user/dashboard.html', {'section': 'dashboard'})
 
 @login_required
 def edit(request):
-	if request.method == 'POST':
-		user_form = UserEditForm(instance=request.user, data=request.POST, files=request.FILES)
-		if user_form.is_valid():
-			user_form.save()
-	else:
-		user_form = UserEditForm(instance=request.user)
-	return render(request, 'user/edit.html',
-					{'user_form': user_form})
+    if request.method == 'POST':
+    user_form = UserEditForm(instance=request.user, data=request.POST, files=request.FILES)
+    if user_form.is_valid():
+    user_form.save()
+    else:
+    user_form = UserEditForm(instance=request.user)
+    return render(request, 'user/edit.html',
+    {'user_form': user_form})
 
 @login_required
 def user_list(request):
-	users = CustomUser.objects.filter(is_active=True)
-	return render(request, 'user/people.html', {'users': users})
+    users = CustomUser.objects.filter(is_active=True)
+    return render(request, 'user/people.html', {'users': users})
 
 @login_required
 def user_detail(request, username):
-	user = get_object_or_404(CustomUser, username=username, is_active=True)
-	return render(request, 'user/profile.html',
-			{'section': 'profile', 'user': user})
+    user = get_object_or_404(CustomUser, username=username, is_active=True)
+    return render(request, 'user/profile.html',
+    {'section': 'profile', 'user': user})
 
 @ajax_required
 @require_POST
 @login_required
 def user_follow(request):
-	user_id = request.POST.get('id')
-	action = request.POST.get('action')
-	if user_id and action:
-		try:
-			user = CustomUser.objects.get(id=user_id)
-			if action == 'follow':
-				Contact.objects.get_or_create(from_user=request.user, to_user=user)
-			else:
-				Contact.objects.filter(from_user=request.user, to_user=user).delete()
-			return JsonResponse({'status':'ok'})
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+    try:
+    user = CustomUser.objects.get(id=user_id)
+    if action == 'follow':
+    Contact.objects.get_or_create(from_user=request.user, to_user=user)
+    else:
+    Contact.objects.filter(from_user=request.user, to_user=user).delete()
+    return JsonResponse({'status':'ok'})
 
-		except CustomUser.DoesNotExist:
-			return JsonResponse({'status':'ko'})
+    except CustomUser.DoesNotExist:
+    return JsonResponse({'status':'ko'})
 
-	return JsonResponse({'status':'ko'})
+    return JsonResponse({'status':'ko'})
