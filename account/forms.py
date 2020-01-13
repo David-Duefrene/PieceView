7db09df806c585a19f53e4d2e41fcd673d2a0bdb
David Duefrene <fortismilite@gmail.com>
2020-01-13 03:29:15 -0700

    Update forms.py
    found 2 failing tests in the account/forms tests TestUserEditForm had picked up the new following attribute and allowed the user to manually edit it. I elected to switch the suer edit form from excluding a list a fields in including fields

--- account/forms.py
+++ account/forms.py
@@ -52,9 +52,7 @@ class UserEditForm(forms.ModelForm):
     """
     class Meta:
         model = get_user_model()
-        exclude = ('last_login', 'is_superuser', 'groups', 'user_permissions',
-                   'is_staff', 'is_active', 'date_joined', 'password',
-                   'username', 'contacts')
+        fields = ('first_name', 'last_name', 'email', 'photo')
 
     def __init__(self, *args, **kwargs):
         super(UserEditForm, self).__init__(*args, **kwargs)
