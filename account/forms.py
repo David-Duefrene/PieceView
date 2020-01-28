8985aab12761bad92e0edca9b5fbb638ca5bb6d3
David Duefrene <fortismilite@gmail.com>
2020-01-27 23:58:54 -0700

    Password Validation
    Discovered a bug where password was not being validated in UserRegistrationForm.
    Fixed it by inheriting from UserCreationForm instead of ModelForm.
    Renamed password to password1 to override UserCreationForm default password fields.
    Removed overridden clean_password2 and save as they were just performing the same things as the UserCreationForm methods.

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import BaseUserManager
 
 
@@ -7,14 +8,14 @@ class UserManager(BaseUserManager):
     pass
 
 
-class UserRegistrationForm(forms.ModelForm):
+class UserRegistrationForm(UserCreationForm):
     """
     Form fora new user to register on the site.
     Required fields are username, email, and passwords.
     """
-    password = forms.CharField(label='Password',
-                               widget=forms.PasswordInput(
-                                attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Password',
+                                widget=forms.PasswordInput(
+                                    attrs={'class': 'form-control'}))
     password2 = forms.CharField(label='Repeat password',
                                 widget=forms.PasswordInput(
                                     attrs={'class': 'form-control'}))
@@ -31,22 +32,6 @@ class UserRegistrationForm(forms.ModelForm):
             'photo': forms.FileInput(
                 attrs={'class': 'form-control custom-file-input'}),
         }
-
-    def clean_password2(self):
-        """Ensures both passwords match."""
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Passwords don\'t match.',
-                                        code='mismatch_pass')
-        return cd['password2']
-
-    def save(self, commit=True):
-        """Saves the form to the database in a hashed format."""
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password"])
-        if commit:
-            user.save()
-        return user
 
 
 class UserEditForm(forms.ModelForm):
