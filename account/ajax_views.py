6f9df01027aa452a8af0d5c3937bb930595cc5a3
David Duefrene <fortismilite@gmail.com>
2020-02-06 05:41:24 -0700

    Pagination Logic
    Wrote most of the logic for paginating followers. Next step is to move it over to a Custom Manager.
    Made followers auto populate when page loads.

--- account/ajax_views.py
+++ account/ajax_views.py
@@ -13,7 +13,6 @@ class GetFollowers(AuthAjaxOnlyMixin):
     """
 
     def post(self, request, *arg):
-        user_id = request.POST.get('id')
         page_limit = int(request.POST.get('page_limit'))
         page_num = int(request.POST.get('page_num'))
         action = request.POST.get('action')
@@ -21,28 +20,49 @@ class GetFollowers(AuthAjaxOnlyMixin):
         try:
             user = request.user
             total_followers = user.followers.count()
-            prev_set = page_limit * (page_num - 1)
+            prev_set = page_limit * (page_num)
 
-            if action == 'next':
+            # both statements keep us in bounds
+            if prev_set < page_limit:
+                action = 'first'
+            if prev_set >= total_followers:
+                action = 'last'
+
+            if action == 'previous':
+                # Going back a page, so we need to back past this page
+                # and go to the begining of the previous page.
+                next_set = prev_set - page_limit * 2
+                page_num -= 1
+                followers = user.followers.order_by("-date_joined")[
+                    next_set:prev_set]
+
+            # user hit next set of cards
+            elif action == 'next':
                 next_set = prev_set + page_limit
                 page_num += 1
-            elif action == 'previous':
-                next_set = prev_set + page_limit
-                page_num -= 1
+                followers = user.followers.order_by("-date_joined")[
+                    prev_set:next_set]
+
+            # user wants 1st set of cards
             elif action == 'first':
                 next_set = page_limit
                 prev_set = 0
-                page_num = 0
+                page_num = 1
+                followers = user.followers.order_by("-date_joined")[
+                    prev_set:next_set]
+
+            # and user wants last set
             elif action == 'last':
                 next_set = total_followers
                 prev_set = total_followers - page_limit
                 page_num = total_followers // page_limit
-            else:
+                followers = user.followers.order_by("-date_joined")[
+                    prev_set:next_set]
+
+            else:  # something has gone haywire if we are here.
                 return JsonResponse({'status': 'ko'})
 
-            followers = user.followers.order_by("-date_joined")[
-                prev_set:next_set]
-
+            # create the dict of followers for the front end.
             followers_dict = []
             for people in followers:
                 followers_dict.append({
@@ -51,6 +71,7 @@ class GetFollowers(AuthAjaxOnlyMixin):
                     'name': str(people),
                     })
 
+            # finally return the data.
             return JsonResponse({
                 'status': 'OK',
                 'followers': followers_dict,
--- static/js/pagination.js
+++ static/js/pagination.js
@@ -1,23 +1,30 @@
+function process_data(url, action){
+  $.post(url, {
+      page_limit: $('button.centered-link').attr('data-limit'),
+      page_num: $('span.current-page').text(),
+      action: action
+    },
+    function(data){
+      if (data['status'] != 'OK') {
+        console.log(data);
+      }
+      for (var i = 0; i < data['followers'].length; i++) {
+        $('#Follower'+[i]+' img.card-img-top').attr("src",
+          data['followers'][i]['photo']);
+        $('#Follower'+[i]+' div.card-footer a.stretched-link').attr("href",
+          data['followers'][i]['url']);
+        $('#Follower'+[i]+' .card-body .card-title').text(
+          data['followers'][i]['name']);
+      }
+      $('span.current-page').text(data['new_page']);
+    }
+  )
+}
+
 function paginateUsers(url) {
   $('button.page-link').click(function(e){
     e.preventDefault();
-    $.post(url, {
-        page_limit: $('button.centered-link').attr('data-limit'),
-        page_num: $('span.current-page').text(),
-        action: $(this).data('action')
-      },
-      function(data){
-        for (var i = 0; i < data['followers'].length; i++) {
-          $('#Follower'+[i]+' img.card-img-top').attr("src",
-            data['followers'][i]['photo']);
-          $('#Follower'+[i]+' div.card-footer a.stretched-link').attr("href",
-            data['followers'][i]['url']);
-          $('#Follower'+[i]+' .card-body .card-title').text(
-            data['followers'][i]['name']);
-        }
+    process_data(url, $(this).data('action'))
 
-        $('span.current-page').text(data['new_page']);
-      }
-    )
   })
 }
