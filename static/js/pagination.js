4da976bf4f5c9e878cfb3dfb89f42e74606543cc
David Duefrene <fortismilite@gmail.com>
2020-02-17 04:41:24 -0700

    template and js done
    template is not setup for following pagination.
    JS can handle it as well.

--- static/js/pagination.js
+++ static/js/pagination.js
@@ -21,8 +21,39 @@ function process_followers(url, action){
   )
 }
 
-function paginateUsers(url) {
-  $('button.page-link').click(function(e){
+function process_following(url, action){
+  $.post(url, {
+      page_limit: $('button.following-page').attr('data-limit'),
+      page_num: $('span.following-current-page').text(),
+      action: action
+    },
+    function(data){
+      if (data['status'] != 'OK') {
+        console.log(data);
+      }
+      for (var i = 0; i < data['followers'].length; i++) {
+        $('#Following'+[i]+' img.card-img-top').attr("src",
+          data['following'][i]['photo']);
+        $('#Following'+[i]+' div.card-footer a.stretched-link').attr("href",
+          data['following'][i]['url']);
+        $('#Following'+[i]+' .card-body .card-title').text(
+          data['following'][i]['name']);
+      }
+      $('span.following-current-page').text(data['new_page']);
+    }
+  )
+}
+
+function paginateFollowers(url) {
+  $('button.followers').click(function(e){
+    e.preventDefault();
+    process_followers(url, $(this).data('action'))
+
+  })
+}
+
+function paginateFollowing(url) {
+  $('button.following').click(function(e){
     e.preventDefault();
     process_followers(url, $(this).data('action'))
 
