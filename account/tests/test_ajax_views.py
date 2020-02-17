f9ae5ddf8a7f584e63dfcd1d1304fe24721615eb
David Duefrene <fortismilite@gmail.com>
2020-02-17 07:31:42 -0700

    More testing
    Added test_post_rejects_bad_request_type and test_following_request_accepted.
    Also added several docstrings.
    Added ability for manager to differ between following and followers.
    Updated variable names accordingly.
    Populate script now generates people to  follow.

--- account/tests/test_ajax_views.py
+++ account/tests/test_ajax_views.py
@@ -7,15 +7,17 @@ import json
 from populate import Populate
 
 
-class GetFollowersTest(TestCase):
-
+class GetUsersTest(TestCase):
+    ''' Class for testing the GetUsers ajax view.'''
     def setUp(self):
+        ''' Creates a user as self.user.'''
         self.user = CustomUser.objects.create_user(
             username='alfred',
             password='Hads65ads1',
         )
 
     def test_post_rejects_bad_data(self):
+        ''' Test to make sure view rejects bad data.'''
         request = {'test': 'bad data'}
         self.client.login(username='alfred', password='Hads65ads1')
         response = self.client.post(
@@ -28,6 +30,7 @@ class GetFollowersTest(TestCase):
         self.assertEqual(data['status'], 'Bad Data: 404')
 
     def test_post_rejects_bad_action(self):
+        ''' Makes sure the view rejects bad actions.'''
         request = {'page_limit': 5, 'page_num': 1, 'user': self.user,
                    'action': 'bad', 'request_type': 'followers'}
         self.client.login(username='alfred', password='Hads65ads1')
@@ -40,7 +43,43 @@ class GetFollowersTest(TestCase):
         data = json.loads(response.content.decode('utf-8'))
         self.assertEqual(data['status'], 'Bad Request: Bad Action.')
 
+    def test_post_rejects_bad_request_type(self):
+        ''' Makes sure the view rejects bad request types.'''
+        request = {'page_limit': 5, 'page_num': 1, 'user': self.user,
+                   'action': 'first', 'request_type': 'bad'}
+        self.client.login(username='alfred', password='Hads65ads1')
+        response = self.client.post(
+            reverse('get_users'),
+            request,
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(data['status'], 'Bad request_type')
+
+    def test_following_request_accepted(self):
+        ''' Make sure the view accepts following as a request_type.'''
+        pop = Populate()
+        pop.users(['10'])
+        pop.following(['10', 'alfred'])
+        request = {'page_limit': 5, 'page_num': 2, 'user': self.user,
+                   'action': 'first', 'request_type': 'following'}
+        self.client.login(username='alfred', password='Hads65ads1')
+
+        response = self.client.post(
+            reverse('get_users'),
+            request,
+            HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.content.decode('utf-8'))
+
+        self.assertEqual('OK', data['status'])
+        self.assertEqual(5, len(data['followers']), 'message')
+        self.assertEqual(1, data['new_page'])
+
     def test_next_action(self):
+        ''' Make sure the view get the next page correctly.'''
         pop = Populate()
         pop.users(['10'])
         pop.followers(['10', 'alfred'])
@@ -60,6 +99,7 @@ class GetFollowersTest(TestCase):
         self.assertEqual(2, data['new_page'])
 
     def test_previous_action(self):
+        ''' Make sure the view get the previous page correctly.'''
         pop = Populate()
         pop.users(['10'])
         pop.followers(['10', 'alfred'])
@@ -80,6 +120,7 @@ class GetFollowersTest(TestCase):
         self.assertEqual(1, data['new_page'])
 
     def test_first_action(self):
+        ''' Make sure the view get the first page correctly.'''
         pop = Populate()
         pop.users(['10'])
         pop.followers(['10', 'alfred'])
@@ -100,6 +141,7 @@ class GetFollowersTest(TestCase):
         self.assertEqual(1, data['new_page'])
 
     def test_last_action(self):
+        ''' Make sure the view get the last page correctly.'''
         pop = Populate()
         pop.users(['10'])
         pop.followers(['10', 'alfred'])
@@ -120,6 +162,7 @@ class GetFollowersTest(TestCase):
         self.assertEqual(2, data['new_page'])
 
     def test_negative_follower_request(self):
+        ''' Make sure the view does not attempt to get a 0 or negative page.'''
         pop = Populate()
         pop.users(['10'])
         pop.followers(['5', 'alfred'])
@@ -140,6 +183,9 @@ class GetFollowersTest(TestCase):
         self.assertEqual(1, data['new_page'])
 
     def test_more_than_total_followers_request(self):
+        '''
+        Make sure the view does not attempt to get a page past whats available.
+        '''
         pop = Populate()
         pop.users(['10'])
         pop.followers(['5', 'alfred'])
--- common/manager.py
+++ common/manager.py
@@ -6,45 +6,53 @@ class PaginateManager(models.Manager):
     display."""
 
     @staticmethod
-    def _get_followers(user, prev_set: int, next_set: int):
+    def _get_followers(user, prev_set: int, next_set: int, request_type: str):
         # cannot be negitive
         if prev_set < 0:
             prev_set = 0
 
-        followers = user.followers.order_by("-date_joined")[
-                    prev_set:next_set]
-        followers_dict = []
-        for people in followers:
-            followers_dict.append({
+        if request_type == 'followers':
+            list = user.followers.order_by("-date_joined")[
+                        prev_set:next_set]
+        elif request_type == 'following':
+            list = user.following.order_by("-date_joined")[
+                        prev_set:next_set]
+        result = []
+        for people in list:
+            result.append({
                 'photo': people.photo_url,
                 'url': people.get_absolute_url(),
                 'name': str(people),
                 })
-        return followers_dict
+        return result
 
 # Removed get_query_set due to Contacts model not having an active status
 
-    def next_set(self, user, page_limit: int, prev_set: int, request_type: str):
+    def next_set(
+            self, user, page_limit: int, prev_set: int, request_type: str):
         """ Grabs the next set of data."""
         next_set = prev_set + page_limit
-        return self._get_followers(user, prev_set, next_set)
+        return self._get_followers(user, prev_set, next_set, request_type)
 
-    def previous_set(self, user, page_limit: int, prev_set: int, request_type: str):
+    def previous_set(
+            self, user, page_limit: int, prev_set: int, request_type: str):
         """ Grabs the previous set of data."""
         # Going back a page, so we need to back past this page
         # and go to the begining of the previous page.
         next_set = prev_set - page_limit
-        return self._get_followers(user, next_set, prev_set)
+        return self._get_followers(user, next_set, prev_set, request_type)
 
-    def first_set(self, user, page_limit: int, prev_set: int, request_type: str):
+    def first_set(
+            self, user, page_limit: int, prev_set: int, request_type: str):
         """ Grabs the first set of data."""
         next_set = page_limit
         prev_set = 0
-        return self._get_followers(user, prev_set, next_set)
+        return self._get_followers(user, prev_set, next_set, request_type)
 
-    def last_set(self, user, page_limit: int,
-                 total_followers: int, prev_set: int, request_type: str):
+    def last_set(
+            self, user, page_limit: int, total_followers: int, prev_set: int,
+            request_type: str):
         """ Grabs the last set of data."""
         next_set = total_followers
         prev_set = total_followers - page_limit
-        return self._get_followers(user, prev_set, next_set)
+        return self._get_followers(user, prev_set, next_set, request_type)
