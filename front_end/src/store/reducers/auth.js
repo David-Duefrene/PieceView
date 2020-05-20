abd4c374cd214ed29f324a6081c3aaedf812970f
David Duefrene <fortismilite@gmail.com>
2020-05-20 01:33:25 -0600

    Update auth.js
    Added documentation and switched quotes double to single

--- front_end/src/store/reducers/auth.js
+++ front_end/src/store/reducers/auth.js
@@ -1,12 +1,24 @@
-import * as actions from "../actions/actionTypes";
+import * as actions from '../actions/actionTypes';
 
+/**
+ * The initial state of the app.
+ * @prop {string} token The user's authentication token.
+ * @prop {bool} isAuthenticated If the user is authenticated.
+ * @prop {bool} isLoading If the app is still loading.
+ * @prop {object} user The object representing the app's user.
+ */
 const initialState = {
-    token: localStorage.getItem("token"),
+    token: localStorage.getItem('token'),
     isAuthenticated: null,
     isLoading: false,
     user: null,
 };
 
+/**
+ * Dispatches the needed action.
+ * @param {object} state The current app state.
+ * @param {action} action The action the app needs to dispatch.
+ */
 export default function(state = initialState, action) {
     switch (action.type) {
         case actions.USER_LOADING:
@@ -23,7 +35,7 @@ export default function(state = initialState, action) {
             };
         case actions.LOGIN_SUCCESS:
         case actions.REGISTER_SUCCESS:
-            localStorage.setItem("token", action.payload.token);
+            localStorage.setItem('token', action.payload.token);
             return {
                 ...state,
                 ...action.payload,
@@ -35,7 +47,7 @@ export default function(state = initialState, action) {
         case actions.LOGOUT_SUCCESS:
         case actions.REGISTER_FAIL:
         case actions.REGISTER_FAIL:
-            localStorage.removeItem("token");
+            localStorage.removeItem('token');
             return {
                 ...state,
                 token: null,
