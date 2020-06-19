bda623b0ae8610fdd0acefca6c2862cc43c16fcd
DavidDuefrene <fortismilite@gmail.com>
2020-06-19 09:15:31 -0600

    Fix incorrect import in LogoutButtonTest. Removed CI = true from docker-compose.yml

--- front_end/src/components/UI/NavItem/LogoutButton/LogoutButton.test.js
+++ front_end/src/components/UI/NavItem/LogoutButton/LogoutButton.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { NavLink } from 'react-router-dom';
 
-import LogoutButton from './logoutButton';
+import LogoutButton from './LogoutButton.js';
+
 
 configure({ adapter: new Adapter() });
 
