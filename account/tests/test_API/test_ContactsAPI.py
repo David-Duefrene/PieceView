ab31c846ba4d162a5868f743f608134ad9080bfd
DavidDuefrene <fortismilite@gmail.com>
2020-07-02 04:56:16 -0600

    From test_ContactsAPI I added documentation, cleaned up whitespaces and newlines, and had initial user get created by directly adding to the DB instead of a client post call.

--- account/tests/test_API/test_ContactsAPI.py
+++ account/tests/test_API/test_ContactsAPI.py
@@ -7,9 +7,14 @@ from account.models import CustomUser
 
 
 class ContactsAPITest(APITestCase):
-    """Test the Contacts API"""
+    """Test the Contacts API.
+        Attributes:
+            user_data: the data to create a user
+        Methods:
+            setup: resets the class attribute back to defaults
+    """
     def setUp(self):
-        self.data = {
+        self.user_data = {
             'username': 'TestUser',
             'email': 'test@test.com',
             'password': 'password',
@@ -18,21 +23,27 @@ class ContactsAPITest(APITestCase):
         }
 
         # Create a user
-        userObject = CustomUser.objects
-        self.client.post(reverse('register_API'), self.data, format='json')
-        self.assertTrue(userObject.get(username=self.data['username']))
+        user = CustomUser.objects.create(
+            username=self.user_data['username'],
+            email=self.user_data['email'],
+            first_name=self.user_data['first_name'],
+            last_name=self.user_data['last_name']
+        )
+        user.set_password(self.user_data['password'])
+        user.save()
+
+        # userObject = CustomUser.objects
+        # self.client.post(reverse('register_API'), self.user_data, format='json')
+        # self.assertTrue(userObject.get(username=self.user_data['username']))
 
     def test_auth_user_gets_contacts_list(self):
         """Tests to ensure a authenticated user gets their contact list"""
         token = self.client.post(
-            reverse('log_API'),
-            {
-                'username': self.data['username'],
-                'password': self.data['password']
-            },
-        ).data['token']
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+            reverse('log_API'), {
+                'username': self.user_data['username'],
+                'password': self.user_data['password'] }).data['token']
 
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
         response = self.client.get(reverse('contacts_API'))
         self.assertEqual(response.status_code, 200)
 
