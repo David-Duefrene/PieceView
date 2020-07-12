a90cec186add810943f34d82ef9b0617d097df0e
DavidDuefrene <fortismilite@gmail.com>
2020-07-12 06:01:37 -0600

    linted reducer/index.js

--- front_end/src/store/reducers/index.js
+++ front_end/src/store/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
 import auth from './auth';
-import messages from "./messages";
+import messages from './messages';
 
 export default combineReducers({ auth, messages });
