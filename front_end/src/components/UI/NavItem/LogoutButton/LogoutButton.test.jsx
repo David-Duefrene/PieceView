40c2deb89836711f3428d78ff9db0ea5a3eea071
DavidDuefrene <fortismilite@gmail.com>
2020-08-16 05:46:17 -0600

    fixed failing test.

--- front_end/src/components/UI/NavItem/LogoutButton/LogoutButton.test.jsx
+++ front_end/src/components/UI/NavItem/LogoutButton/LogoutButton.test.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import LogoutButton from './LogoutButton';
 
 configure({ adapter: new Adapter() });
+const mockStore = configureMockStore([thunk]);
 
 describe('<LogoutButton />', () => {
     let wrapper;
+    const store = mockStore({});
 
     beforeEach(() => {
-        const log = () => {};
-        wrapper = shallow(<LogoutButton logoutReducer={log} />);
+        wrapper = mount(<Router><Provider store={store}><LogoutButton /></Provider></Router>);
     });
 
     it('should have 1 <a />', () => {
-        expect(wrapper.find('a')).toHaveLength(1);
-    });
-
-    it('should have onClick property of test', () => {
         expect(wrapper.find('a')).toHaveLength(1);
     });
 });
