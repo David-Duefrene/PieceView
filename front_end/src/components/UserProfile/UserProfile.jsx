9728fb06f48a278b30e5620b90739608b6a8dd03
DavidDuefrene <fortismilite@gmail.com>
2020-08-17 01:32:58 -0600

    User profile page shows a user card, redid the card component to check the current url and not show  aprofile link if on a profile.

--- front_end/src/components/UserProfile/UserProfile.jsx
+++ front_end/src/components/UserProfile/UserProfile.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
-import dotenv from 'dotenv';
 
+import Card from '../CardDeck/Card/Card';
 import axios from '../../axios';
 import CSS from './UserProfile.module.css';
-
-const config = dotenv.config();
 
 /**
  * Renders a user profile for an individual user
@@ -34,18 +32,19 @@ export class UserProfile extends Component {
         const { isLoaded, user } = this.state;
 
         return (
-            <div>
-                { isLoaded ? (
-                    <div className={CSS.UserProfile}>
-                        <h1>{user.username}</h1>
-                        <h3>{`Name: ${user.first_name} ${user.last_name}`}</h3>
-                        <img src={`${process.env.REACT_APP_API_URL}${user.photo_url}`} />
-                        <p>{user.biography}</p>
-                    </div>
-                ) : (<h1>Loading!!!</h1>)}
+            <div className={CSS.Profile}>
+                { isLoaded ? <Card user={user} /> : (<h1>Loading!!!</h1>)}
             </div>
         );
     }
 }
+
+UserProfile.propTypes = {
+    match: PropTypes.shape({
+        params: PropTypes.shape({
+            pk: PropTypes.number.isRequired,
+        }).isRequired,
+    }).isRequired,
+};
 
 export default withRouter(UserProfile);
